2c5122a8b4a2dff9717c8759da78891f10f2d1d9
Cywil <tomaszprogramowanie@gmail.com>
2016-11-28 00:42:47 +0100

    Update and rename .gitignore to script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+(function($){
+
+$(document).ready(function(){
+
+	var divGrid = $("div .grid.grid-12");
+	var aHref = $("nav a[href^='http']");
+	var input = $(":radio:not(:checked), :checkbox:not(:checked)");
+  var pFind= $("div[type='text'] p:empty:first");
+	var spanP = $(":not(span).pagination-item");
+
+})
+
+})(jQuery);
